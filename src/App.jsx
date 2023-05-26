ca2b7abfcebb289634521ffed89eb180e6602316
Silmara Sozza <silmara.sozza@gmail.com>
2023-05-26 16:00:58 -0300

    Interando Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,23 +44,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+         {posts.map( post => {
+          return (
           <Post
-            author="Diego Fernandes"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+
           />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
-          {posts.map(post => {
-            return (
-              <Post
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            )
-          })}
+          )
+         })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,9 @@ import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post(props) {
+console.log(props);
+
   return (
     <article className={styles.post}>
       <header>
@@ -18,14 +20,7 @@ export function Post() {
         <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        
       </div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
