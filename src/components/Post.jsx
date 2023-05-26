e5f86c0b6c2e0d48f545eefd42620ce054e6fa89
Silmara Sozza <silmara.sozza@gmail.com>
2023-05-26 17:33:30 -0300

    mod 3

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,17 +4,15 @@ import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(props) {
-console.log(props);
-
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <img className={styles.avatar}  />
-          <Avatar src="https://github.com/diego3g.png" />
+          <Avatar src= {props.author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Diego Fernandes</strong>
-            <span>Web Developer</span>
+            <strong> {props.author.name}</strong>
+            <span>{props.author.role}</span>
           </div>
         </div>
         <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
