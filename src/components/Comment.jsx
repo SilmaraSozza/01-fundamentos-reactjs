f9757356b128c8be98b1b27cd31d86123453d930
Silmara Sozza <silmara.sozza@gmail.com>
2023-05-29 18:25:12 -0300

    aplaudir

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+
  function handleDeleteComment() {
     onDeleteComment(content);
+ }
+
+ function handleLikeComment() {
+  setLikeCount(likeCount + 1);
  }
 
   return (
@@ -21,7 +29,7 @@ export function Comment({ content, onDeleteComment }) {
             </div>
 
             <button onClick={handleDeleteComment} title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           
@@ -30,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
